30f59963d6704566b7e25f660600d268ce7ff653
Walid Moussa <walidmoussa@gmail.com>
2025-09-30 11:04:15 +0300

    solution

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export async function connectDB() {
   const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error('MONGO_URI is not set');
+  if (!uri) throw new Error("MONGO_URI is not set");
   try {
     await mongoose.connect(uri, { autoIndex: true });
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error('MongoDB connection error', err.message);
+    console.error("MongoDB connection error", err.message);
     process.exit(1);
   }
 }
